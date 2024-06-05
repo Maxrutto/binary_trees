c6b20449850cdcfad863d6c7ddf7b221417929b2
Maxrutto <ruttomaxmillan@gmail.com>
2024-06-05 12:40:36 -0700

    Insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -32,10 +32,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		parent->left = tempNode;
 	}
 	else
-	{
 		tempNode->n = value;
-		parent->left = tempNode;
-	}
+	parent->left = tempNode;
 
 	return (tempNode);
 }
